87d5e51a6dc1e9aad90d4eddc4abf9f48d8a953b
apzl <apsalshbk550@gmail.com>
2020-08-18 22:16:55 +0530

    final

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+[![Build Status](https://travis-ci.org/{apzl}/{mask-or-not}.png?branch=master)](https://travis-ci.org/{apzl}/{mask-or-not})
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/dwyl/esta/issues)
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+[![HitCount](http://hits.dwyl.com/apzl/mask-or-not.svg)](http://hits.dwyl.com/apzl/mask-or-not)
+
 # mask-or-not
+
+Simple mask detection app made using [fast.ai vision](https://docs.fast.ai/vision.html) & [streamlit.io](https://www.streamlit.io/) :sparkling_heart:.
+
+find more details in the notebook.
+
+*wear a mask, stop the spread* :mask:
+
+
+![demo image](mask-or-not.png).
+
+
+PS: Don't mind the accuracy.
--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import torchvision.transforms as T
 
 path = Path(__file__).parent
 
-
+#cached to avoid reloading the model
 @st.cache(allow_output_mutation = True)
-def learner(path):
-	learn = load_learner(path)
+def learner(path):						#load the learner from 'export.pkl'
+	learn = load_learner(path)	
 	return learn
 
-learn = learner(path)
+
 
 def main():
 	st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -23,17 +23,17 @@ def main():
 	"""
 	st.markdown(html_title, unsafe_allow_html = True)
 	st.subheader("Detect whether masks are worn properly")
+	learn = learner(path)
 	uploaded_file = st.file_uploader("Choose a image file", type=['png', 'jpg', 'jpeg'])
 	if uploaded_file is not None:
-		img_pil = PIL.Image.open(uploaded_file)
-		img_tensor = T.ToTensor()(img_pil)
-		our_image = Image(img_tensor)
 		st.image(uploaded_file, width=200)
-		pred = learn.predict(our_image)[0]
+		our_image = open_image(uploaded_file)	#converting to an Image object		
+		pred = learn.predict(our_image)[0]	
 		st.success(pred)
 	footer = """
 	<div style="position:fixed; text-align:center; bottom:0px; right:0px; left:0px; background-color:grey" markdown="1">
-		<h3>Built with Streamlit using fast.ai</h3>
+		<h4>Built with Streamlit using <a href= "https://www.fast.ai">fast.ai</a></h4>
+		<p><a href="https://github.com/apzl/mask-or-not">Github</a> | &copyapsal</p>
 	</div>
 	"""
 	st.markdown(footer, unsafe_allow_html=True)
